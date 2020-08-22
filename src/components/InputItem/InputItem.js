c49a83ff80ebe92ef59e53e0105da79152bf0a45
Дорошенко Алина <belyanina.07@mail.ru>
2020-08-22 09:30:19 +0300

    add new component TextField

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<span>Новое дело</span>);
+const InputItem = () => (<span>
+	<TextField
+          id="standard-password-input"
+          label="Новое дело"
+          type="password"
+          autoComplete="current-password"
+    />
+</span>);
 
 export default InputItem;
