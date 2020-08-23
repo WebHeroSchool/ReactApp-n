ba2879340b5aa6814857942100798c0d8332cf53
Дорошенко Алина <belyanina.07@mail.ru>
2020-08-23 13:02:08 +0300

    change file InputItem

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<span>
-	<TextField
-          label="Новое дело"
-          id="margin-normal"
-          margin="normal"
+const InputItem = () => (<span className={styles.input}>
+	<TextField 
+        label="Новое дело"
+        className={styles.text}
     />
 </span>);
 
