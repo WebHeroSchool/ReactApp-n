d9bfacb7fbf52d9d4a5e15141110e3647239301e
Дорошенко Алина <belyanina.07@mail.ru>
2020-12-19 13:58:27 +0300

    feat: add fetch

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,70 @@
 import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { Octokit } from '@octokit/rest';
 import styles from './About.module.css';
 
-const About = () => (
-	<div className={styles.wrap}>
-		<div className={styles.about}>
-			<p className={styles.title}>Алина</p>
-		</div>
-	</div>);
+const octokit = new Octokit();
+class About extends React.Component {
+	state = {
+		isLoading: true,
+		repoList: [],
+		avatarUrl: '',
+		nameUser: '',
+		profileUrl: '',
+		fetchFailure: false
+	}
+
+	componentDidMount() {
+		octokit.users.getByUsername({
+		  username: 'Alina1317'
+		}).then(({data}) => {
+			this.setState({
+				avatarUrl: data.avatar_url,
+				nameUser: data.name,
+				profileUrl: data.html_url
+			})
+		}).catch(() => {
+			this.setState({
+				fetchFailure: true
+			})
+		});
+
+		octokit.repos.listForUser({
+			username: 'Alina1317'
+		}).then(({ data }) => {
+			this.setState ({
+				repoList: data,
+				isLoading: false
+			})
+		}).catch(() => {
+			this.setState({
+				fetchFailure: true
+			})
+		});
+	} 
+
+	render() {
+		const { isLoading, avatarUrl, nameUser, profileUrl, repoList, fetchFailure } = this.state;
+		return (
+			<div className={styles.wrap}>
+				<div className={styles.about}>
+					<h1 className={styles.title}>{ isLoading ? <CircularProgress /> : 'Обо мне' }</h1>
+					<img className={styles.img} src={ avatarUrl } alt="avatar" />
+					<h2 className={styles.name}>{nameUser}</h2>
+					<a href={profileUrl} target="_blank"><button className={styles.button}>Мой профиль на GitHub</button></a>
+					{!isLoading && <ol>
+						{repoList.map(repo => (<li key={repo.id}><a href={repo.html_url} target="_blank">
+							{repo.name}
+						</a>
+						</li>))}
+					</ol>}
+					{fetchFailure && <h2>Ошибка, что-то пошло не так</h2>}
+				</div>
+			</div>
+		);
+	}
+	
+}
+	
 
 export default About;
