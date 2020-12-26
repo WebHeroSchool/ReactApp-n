b357bf95d2265432f49e5ff49655b912cb6c1e91
Дорошенко Алина <belyanina.07@mail.ru>
2020-12-26 17:59:45 +0300

    Updates

---
+++ precache-manifest.f9a86852a72159d4fa56b3ad6a1bbaa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e6f297dec1269ce3833cffdb657bf3",
+    "url": "/AppTODO/index.html"
+  },
+  {
+    "revision": "9f42c2c1c2a06f858904",
+    "url": "/AppTODO/static/css/main.063a3a9e.chunk.css"
+  },
+  {
+    "revision": "3e74d5c05cbca9412990",
+    "url": "/AppTODO/static/js/2.993a610b.chunk.js"
+  },
+  {
+    "revision": "ea400bfb7cfb62de59f10ac82e4a6f9d",
+    "url": "/AppTODO/static/js/2.993a610b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f42c2c1c2a06f858904",
+    "url": "/AppTODO/static/js/main.af057c5a.chunk.js"
+  },
+  {
+    "revision": "e492ebe7bcb28a15be58",
+    "url": "/AppTODO/static/js/runtime-main.d094eb4a.js"
+  }
+]);
